461c10b9bf0f44ece0d1812d3cfa78cf6d73bf2c
lalit-gautam <126085309+lalit-gautam@users.noreply.github.com>
2024-11-27 15:27:45 +0530

    refactor

--- src/main/java/com/security/service/CustomUserDetailsService.java
+++ src/main/java/com/security/service/CustomUserDetailsService.java
@@ -2,11 +2,9 @@ package com.security.service;
 
 import com.security.entity.User;
 import com.security.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
